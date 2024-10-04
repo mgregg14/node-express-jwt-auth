3ef67d9ef6c9278a563cec126f1dda31f5477b5a
Morgan <mgregg14@ivytech.edu>
2024-10-04 12:09:47 -0400

    authRoutes router set up

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,11 @@
 const {Router} = require ('express');
+const authController = require('../controllers/authController');
 const router = Router ();
 
 router.get('/signup',() => {});
-router.get('/signup',() => {});
-router.get('/signup',() => {});
-router.get('/signup',() => {});
-
-
-
-
+router.post('/signup',() => {});
+router.get('/login',() => {});
+router.post('/login',() => {});
 
 module.exports = router;
 
