e8bbefe363771673e6b5a72ae290b62e144128fc
Morgan <mgregg14@ivytech.edu>
2024-10-04 14:48:41 -0400

    return erros

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -5,12 +5,17 @@ const handleErrors= (err) =>{
     console.log(err.message, err.code)
     let errors = {email:'',password:''};
 
+    //duplicate error code
+    if(err.code ===11000){
+        errors.email = "that email is already registered";
+    return errors;
+    }
+
     //validation errors
     if (err.message.includes('User validation failed')){
         Object.values(err.errors).forEach(({properties}) =>{
             errors[properties.path]= properties.message;
         })
- 
     }
 
     return errors;
