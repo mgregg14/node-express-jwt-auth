fb7a1580878a1cd5ef07ef2dd58b6dbe0720b649
Morgan <mgregg14@ivytech.edu>
2024-10-04 12:00:08 -0400

    Create routes folder

---
+++ routes/authRoutes.js
@@ -0,0 +1,14 @@
+const {Router} = require ('express');
+const router = Router ();
+
+router.get('/signup',() => {});
+router.get('/signup',() => {});
+router.get('/signup',() => {});
+router.get('/signup',() => {});
+
+
+
+
+
+module.exports = router;
+
