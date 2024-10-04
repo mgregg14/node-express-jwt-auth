9b18ca5fbb0dd72e238c0e9f7429f155312363a7
Morgan <mgregg14@ivytech.edu>
2024-10-04 19:21:09 -0400

    create error

--- models/User.js
+++ models/User.js
@@ -29,9 +29,13 @@ userSchema.pre('save', async function(next) {
 userSchema.statics.login = async function(email,password){
   const user= await this.findOne({email:email});
   if(user){
-    bcrypt.compare(password, user.password)
+   const auth = await bcrypt.compare(password, user.password);
+   if(auth){
+    return user;
+   }
+   throw Error('Incorrect password');
   }
-  throw Error('Incorrect email')
+  throw Error('Incorrect email');
 }
 
 const User = mongoose.model('user', userSchema);
