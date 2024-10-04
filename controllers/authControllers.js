5dfd0d7ef75a4e622a5a4e5f96c37091e19a9210
Morgan <mgregg14@ivytech.edu>
2024-10-04 14:43:48 -0400

    Properties path

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -3,14 +3,17 @@ const User= require('../models/User');
 //handle errors
 const handleErrors= (err) =>{
     console.log(err.message, err.code)
-    let error = {email:'',password:''};
+    let errors = {email:'',password:''};
 
     //validation errors
     if (err.message.includes('User validation failed')){
-        Object.values(err.errors).forEach(error =>{
-            console.log(error.properties);
+        Object.values(err.errors).forEach(({properties}) =>{
+            errors[properties.path]= properties.message;
         })
+ 
     }
+
+    return errors;
 }
 
 module.exports.signup_get = (req, res) => {
@@ -34,7 +37,7 @@ module.exports.signup_post = async(req, res) => {
 
     catch (err){
         const errors = handleErrors(err);
-        res.status(400).send('error, user not created');
+        res.status(400).json({errors});
 
     }
 }
