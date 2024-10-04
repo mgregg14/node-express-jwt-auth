695982e430ec0986550e8d1e3983c97681889ba0
Morgan <mgregg14@ivytech.edu>
2024-10-04 13:16:23 -0400

    added console.log

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -7,10 +7,13 @@ module.exports.login_get = (req, res) => {
 }
 
 module.exports.signup_post = (req, res) => {
-    res.send('new signup');
+    const {email,password} = req.body;
+    console.log(email,password);
+    res.send('user login');
 }
 
-module.exports.login_post = (req, res) => {
-    console.log(req.body);
+ module.exports.login_post = (req, res) => {
+    const {email,password} = req.body;
+    console.log(email,password);
     res.send('user login');
 }
