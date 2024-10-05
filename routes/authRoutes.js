d5bca4752355327e458ed1fbd3016d0943fa8331
Morgan <mgregg14@ivytech.edu>
2024-10-04 21:03:32 -0400

    conditional rendering

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,6 +6,8 @@ const router = Router();
 router.get('/signup', authController.signup_get);
 router.post('/signup', authController.signup_post);
 router.get('/login', authController.login_get);
-router.post('/login', authController.login_post);
-router.post('/logout', authController.logout_get);
+router.get('/logout', authController.logout_get);
+
 module.exports = router;
+
+
